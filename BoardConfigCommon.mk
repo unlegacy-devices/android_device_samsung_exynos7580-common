df8dcbde30ba7b958e0a73222b11c6530b45fa2e
NotNoelChannel <notnoel12345678@gmail.com>
2022-12-19 14:34:59 +0100

    exynos7580-common: Get ready for Lollipop (cm-12.1)

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -116,7 +116,7 @@ TARGET_KERNEL_ARCH := arm64
 ifeq ($(FORCE_32_BIT),true)
 TARGET_KERNEL_HEADER_ARCH := arm
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
-KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/aarch64/aarch64-linux-android-4.9/bin
+KERNEL_TOOLCHAIN := /opt/toolchain/aarch64-linux-android-4.9/bin
 endif
 BOARD_KERNEL_BASE := 0x10000000
 BOARD_KERNEL_IMAGE_NAME := Image
